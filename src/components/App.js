38b3408a339496e378fa4f9aa3d00a2db7fc0397
iradukundacyuzuzo50@gmail.com <root@Andelas-MacBook-Pro.local>
2022-04-13 13:02:31 +0200

    changinge Build node version to 14.x

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1> Avengers phantom</h1>
+      <p>Hello Avengers FE</p>
+    </div>
+  );
+}
+
+
+
+export default App;
