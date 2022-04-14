65798612166f7d2b4e3ee9f327c92535a28adfa7
kareraolivier <kareraolivier@gmail.com.com>
2022-04-14 18:04:14 +0200

    adding dummy route

---
+++ src/Components/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About phantom project</h1>
+      <a href="/">Home</a>
+    </div>
+  );
+}
+
+export default About;
