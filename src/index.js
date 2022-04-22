a2fd34f42c9e2826b88985520218e3754a826a13
andela <andela@andelas-MacBook-Pro.local>
2022-04-22 09:47:45 +0200

    Add Navbar with Dummy Link

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import store from './redux/store.js'
+<<<<<<< HEAD
 import Home from './Components/Homepage/Home.jsx';
+=======
+import Home from './Components/Home.jsx';
+import About from './Components/About.jsx';
+>>>>>>> Add Navbar with Dummy Link
 import Redux from './Components/Redux.jsx';
 import Navbar from './Components/navbar/Navbar.jsx';
 import ScrollToTop from './Components/Homepage/ScrollToTop.jsx'
@@ -12,14 +17,14 @@ import ScrollToTop from './Components/Homepage/ScrollToTop.jsx'
 export default function App() {
   return (
     <BrowserRouter>
-    <ScrollToTop>
-      <Routes>
-        <Route path="/" element={<Home />}>
-        <Route index element={<Home />} />
-        </Route>
-        
-      </Routes>
-    </ScrollToTop>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route index element={<Home />} />
+          </Route>
+
+        </Routes>
+      </ScrollToTop>
     </BrowserRouter>
   );
 }
