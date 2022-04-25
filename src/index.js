8a8d3cb1e5d036a4ea159d7703bb846d9c5c54a7
iradukundacyuzuzo50@gmail.com <root@Andelas-MacBook-Pro.local>
2022-04-25 23:31:58 +0200

    Product landing page# This is a combination of 2 commits.
    Reset password pages

--- src/index.js
+++ src/index.js
@@ -3,19 +3,23 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import store from './redux/store.js'
-import Home from './Components/Home.jsx';
+import Home from './Components/Homepage/Home.jsx';
 import Redux from './Components/Redux.jsx';
 import Navbar from './Components/navbar/Navbar.jsx';
+import ScrollToTop from './Components/Homepage/ScrollToTop.jsx'
 
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Navbar />}>
-       <Route index element={<Home />} />
-      </Route>
-    </Routes>
+    <ScrollToTop>
+      <Routes>
+        <Route path="/" element={<Home />}>
+        <Route index element={<Home />} />
+        </Route>
+        
+      </Routes>
+    </ScrollToTop>
     </BrowserRouter>
   );
 }
