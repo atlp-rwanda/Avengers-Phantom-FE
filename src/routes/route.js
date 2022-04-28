a8c5a66bc5c72fe315bb33b964c9e53c619f464e
kareraolivier <kareraolivier@gmail.com.com>
2022-04-28 16:15:06 +0200

    merged dashboard

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,6 +4,8 @@ import Dashboard from "../Components/dashboard/dashboard/Dashboard.jsx";
 import Drivers from "../Components/dashboard/operatorsanddriver/Drivers.jsx";
 import AddDrivers from "../Components/dashboard/operatorsanddriver/AddDrivers.jsx";
 import Operators from "../Components/dashboard/operatorsanddriver/Operators.jsx";
+import ResetPage from "../Components/reset/ResetPage.jsx";
+import ResetPasswordPage from "../Components/reset/ResetPassword.jsx";
 import AddOperator from "../Components/dashboard/operatorsanddriver/AddOperator.jsx";
 import DriverAndOperator from "../Components/dashboard/operatorsanddriver/DriverAndOperator.jsx";
 import Roles from "../Components/dashboard/roles&permission/Roles.jsx";
@@ -12,39 +14,52 @@ import Routecar from "../Components/dashboard/route/Route.jsx";
 import AppBar from "../Components/dashboard/dashnavbar/DashNavBar.jsx";
 import DashboardSidebar from "../Components/dashboard/sidebar/Sidebar.jsx";
 import Home from "../Components/Homepage/Home.jsx";
+import { useLocation } from "react-router-dom";
 import "../Components/dashboard/Dashboard.css";
 
 const PhantomRoutes = () => {
+  const location = useLocation();
   return (
     <div>
       <Routes>
-        <Route path="/" inedx element={<Home />} />
+        <Route path="/" index element={<Home />} />
       </Routes>
-      <AppBar />
-      <div className="dashboard">
-        <div className="generalmenubar">{<DashboardSidebar />}</div>
+      <Routes>
+        <Route path="/resertpassword" element={<ResetPage />} />
+        <Route path="/resertpasswordpage" element={<ResetPasswordPage />} />
+      </Routes>
+      {location.pathname === "/dashboard" && (
+        <>
+          <AppBar />
+          <div className="dashboard">
+            <div className="generalmenubar">{<DashboardSidebar />}</div>
 
-        <div className="containt">
-          <Routes>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/routes" element={<Routecar />} />
-            <Route path="/driveroperator" element={<DriverAndOperator />} />
-            <Route path="/driveroperator/driver" element={<Drivers />} />
-            <Route
-              path="/driveroperator/driver/adddriver"
-              element={<AddDrivers />}
-            />
-            <Route path="/driveroperator/operator" element={<Operators />} />
-            <Route
-              path="/driveroperator/operator/addoperator"
-              element={<AddOperator />}
-            />
-            <Route path="/bus" element={<Bus />} />
-            <Route path="/rolepermission" element={<Roles />} />
-            <Route path="/bus" component={<Bus />} />
-          </Routes>
-        </div>
-      </div>
+            <div className="containt">
+              <Routes>
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/routes" element={<Routecar />} />
+                <Route path="/driveroperator" element={<DriverAndOperator />} />
+                <Route path="/driveroperator/driver" element={<Drivers />} />
+                <Route
+                  path="/driveroperator/driver/adddriver"
+                  element={<AddDrivers />}
+                />
+                <Route
+                  path="/driveroperator/operator"
+                  element={<Operators />}
+                />
+                <Route
+                  path="/driveroperator/operator/addoperator"
+                  element={<AddOperator />}
+                />
+                <Route path="/bus" element={<Bus />} />
+                <Route path="/rolepermission" element={<Roles />} />
+                <Route path="/bus" component={<Bus />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
