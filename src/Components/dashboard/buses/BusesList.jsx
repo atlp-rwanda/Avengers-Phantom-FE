fd1f72280fca79749a8a59b5f7a56fc27ceff293
Gedeonir <irafasha.jedy12@gmail.com>
2022-04-29 20:10:35 +0200

    CRUD Operations for buses

--- src/Components/dashboard/buses/BusesList.jsx
+++ src/Components/dashboard/buses/BusesList.jsx
@@ -169,6 +169,90 @@ const Buses = () => {
             </Box>
           </Card>
 
+          <Card sx={{ marginBottom: 3 }}>
+            <Box sx={{ display: 'grid', gridTemplateColumns: { lg: '200px 200px 200px 200px', xs: '100%', md: '400px 400px' } }}>
+              <Box
+                component='img'
+                src={Photo}
+                alt='bus image'
+                sx={{ width: { lg: 200, xs: 1 }, height: { lg: 130, xs: 200 } }} />
+              <Box
+                component="div" sx={{ display: 'block', textAlign: 'center', p: 2 }}>
+                <Typography component="label" sx={{ color: '#675B5B', textTransform: 'uppercase', textAlign: 'center', fontWeight: 700, fontSize: { xs: 12, lg: 15 } }}>
+                  Capacity
+                </Typography>
+                <Typography component="p" sx={{ fontWeight: 700, fontSize: { lg: 13, xs: 10 } }}>
+                  100
+                </Typography>
+                <Typography component="label" sx={{ textTransform: 'uppercase', color: '#675B5B', fontSize: { lg: 12, xs: 9 } }}>
+                  Passengers
+                </Typography>
+              </Box>
+              <Box
+                component="div"
+                sx={{ display: 'block', textAlign: 'center', p: 2 }}
+              >
+                <Typography component="label" sx={{ color: '#675B5B', textTransform: 'uppercase', textAlign: 'center', fontWeight: 700, fontSize: { xs: 12, lg: 15 } }}>
+                  Vehicle type
+                </Typography>
+                <Typography component="p" sx={{ fontWeight: 700, fontSize: { lg: 13, xs: 10 } }}>
+                  3B
+                </Typography>
+                <Typography component="label" sx={{ textTransform: 'uppercase', color: '#675B5B', fontSize: { lg: 12, xs: 9 } }}>
+                  type
+                </Typography>
+              </Box>
+              <Box
+                component="div"
+                sx={{ display: 'block', textAlign: 'center', p: 2 }}
+              >
+                <Typography component="label" sx={{ color: '#675B5B', textTransform: 'uppercase', textAlign: 'center', fontWeight: 700, fontSize: { xs: 12, lg: 15 } }}>
+                  Plate number
+                </Typography>
+                <Typography component="p" sx={{ fontWeight: 700, fontSize: { lg: 13, xs: 10 } }}>
+                  RAB 100 C
+                </Typography>
+              </Box>
+            </Box>
+            <Box sx={{ display: 'grid', gridTemplateColumns: { lg: '150px 100px', xs: '100px 50px' }, marginLeft: 'auto', gridGap: '1.3rem', width: { lg: 280, xs: 200 } }}>
+              <Button
+                size="small"
+                component={Link} to='/bus/1'
+                sx={{
+                  background: '#012241',
+                  borderTopLeftRadius: 10,
+                  borderBottomLeftRadius: 0,
+                  borderTopRightRadius: 10,
+                  borderBottomRightRadius: 0,
+                  color: 'white',
+                  fontSize: { lg: 10, xs: 7 },
+                  p: 1,
+                  height: 20,
+                  '&:hover': { background: '#012241', opacity: 0.8, transition: '0.8s' }
+                }}
+              >
+                view full details
+              </Button>
+              <Button
+                onClick={handleClickOpen}
+                sx={{
+                  background: 'rgba(221, 49, 49, 0.91)',
+                  color: 'white',
+                  fontSize: { lg: 10, xs: 7 },
+                  p: 1,
+                  borderTopLeftRadius: 10,
+                  borderBottomLeftRadius: 0,
+                  borderTopRightRadius: 10,
+                  borderBottomRightRadius: 0,
+                  height: 20,
+                  '&:hover': { background: 'rgba(221, 49, 49, 0.91)', opacity: 0.8, transition: '0.8s' }
+                }}
+              >
+                Delete
+              </Button>
+            </Box>
+          </Card>
+
         </Box>
         <Pagination size="small" count={10} color='primary'></Pagination>
         <Dialog
