249f99064f30ee4212d8b0f94f103da35d143b97
N Armstrong <capkrypton@gmail.com>
2022-04-29 09:44:39 +0200

    resolving errors
    rsolves
    
    dashboard container

--- src/index.js
+++ src/index.js
@@ -1,13 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './redux/store.js';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import store from './redux/store.js'
+import Home from './Components/Homepage/Home.jsx';
+// import Redux from './Components/Redux.jsx';
+import Navbar from './Components/navbar/Navbar.jsx';
+import ScrollToTop from './Components/Homepage/ScrollToTop.jsx'
+import  {Signin} from "./Components/signin/Signin.jsx";
+import  {Roles} from "./Components/Roles&permision/Roles.jsx";
+import  ResetPage from "./Components/reset/ResetPage.jsx";
+import  {MaterialUI} from "./Components/Dashboardcont/container.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById('app')).render(
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    {/* <ScrollToTop>
+      <Routes>
+        <Route path="/" element={<Home />}>
+        <Route index element={<Home />} />
+        </Route>
+        
+      </Routes>
+    </ScrollToTop> */}
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+       <Route index element={<Home/>} />
+       <Route path='/HOME' element={<Home/>}/>
+       <Route path='/reset' element={<ResetPage/>}/>
+       <Route path='/signin' element={<Signin/>}/>
+       <Route path='/SERVICES' element={<Roles/>}/>
+       
+       
+      </Route>
+      <Route path="/dashboard" element={<MaterialUI/>}>
+         <Route path='/dashboard/roles' element={<Roles/>}/>
+         <Route path='/dashboard/buses' element={<Roles/>}/>
+         <Route path='/dashboard/operation' element={<Roles/>}/>
+         <Route path='/dashboard/routes' element={<Roles/>}/>
+         <Route path='/dashboard/dashboard' element={<MaterialUI/>}/>
+      </Route>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </Provider>
+  </Provider>,
+  document.getElementById("app")
 );
--- webpack.config.js
+++ webpack.config.js
@@ -3,46 +3,51 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   //This property defines where the application starts
-  entry: './src/index.js',
-
+  entry:'./src/index.js',
+    
   //This property defines the file path and the file name which will be used for deploying the bundled file
-  output: {
+  output:{
     path: path.join(__dirname, '/dist'),
     filename: 'bundle.js',
     publicPath: '/'
+  },
+  devServer: {
+    // contentBase: path.join(__dirname, 'src'),
+    historyApiFallback: true,
+
   },
   //Setup loaders
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|jsx)$/, 
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
         }
       },
-
       {
         test: /\.(css|scss)$/,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: "sass-loader"
+          }
+        ],
         exclude: /\.module\.css$/
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource'
+        type: 'asset/resource',
       }
     ]
   },
-
+    
   // Setup plugin to use a HTML file for serving bundled js files
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
     })
-  ],
-  devServer: {
-    static: path.resolve(__dirname, './dist'),
-    hot: true,
-    historyApiFallback: true
-  }
-};
+  ]
+}
