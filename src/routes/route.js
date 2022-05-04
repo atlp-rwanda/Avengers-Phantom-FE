971c94bbdb0b20771e8ebdc3aa422182254e45a9
kareraolivier <kareraolivier@gmail.com.com>
2022-05-04 09:07:10 +0200

    well done dashboard

--- src/routes/route.js
+++ src/routes/route.js
@@ -64,7 +64,7 @@ const PhantomRoutes = () => {
           }
         />
         <Route
-          path="/contact%20US"
+          path="/contact"
           element={
             <>
               <Navbar />
