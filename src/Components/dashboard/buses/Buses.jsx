d89eaef9a7d24410a3ecc00ac84f1c40fda19f3e
kareraolivier <kareraolivier@gmail.com.com>
2022-05-04 17:22:09 +0200

    well done dashboard

--- src/Components/dashboard/buses/Buses.jsx
+++ src/Components/dashboard/buses/Buses.jsx
@@ -1,15 +1,11 @@
 import React from "react";
-<<<<<<< HEAD
 import Sidebar from "../sidebar/Sidebar.jsx";
 import DashNavbar from "../dashnavbar/DashNavBar.jsx";
 import "../Dashboard.css";
-=======
->>>>>>> 951c7c2f (hello)
 
 const Buses = () => {
   return (
     <div>
-<<<<<<< HEAD
       <DashNavbar />
       <div className="dashboard">
         <div className="generalmenubar">{<Sidebar />}</div>
@@ -17,9 +13,6 @@ const Buses = () => {
           <h1>Buses</h1>
         </div>
       </div>
-=======
-      <h1>buses</h1>
->>>>>>> 951c7c2f (hello)
     </div>
   );
 };
