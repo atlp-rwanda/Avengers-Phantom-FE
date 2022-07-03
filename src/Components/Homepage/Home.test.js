671ec3b9e847f293a4aee502612ef871a92decbd
kareraolivier <kareraolivier@gmail.com>
2022-07-03 22:01:20 +0200

    tests

--- src/Components/Homepage/Home.test.js
+++ src/Components/Homepage/Home.test.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import  Home  from "./Home";
+import { render, screen } from "@testing-library/react";
+import Home from "./Home";
 
+// describe("App Component", function () {
+//     it("should return Webpack phantom landing page", function () {
+//       let { getByText } = render(<Home/>);
+//       expect(getByText("Webpack phantom landing page")).toMatchInlineSnapshot(`
+//       <h1>
+//         Webpack phantom landing page
+//       </h1>
+//       `);
+//     });
+//   });
 
-describe("App Component", function () {
-    it("shoul return Webpack phantom landing page", function () {
-      let { getByText } = render(<Home/>);
-      expect(getByText("Webpack phantom landing page")).toMatchInlineSnapshot(`
-      <h1>
-        Webpack phantom landing page
-      </h1>
-      `);
-    });
-  });
+test("should return Webpack phantom landing page", () => {
+  render(<Home />);
+  const homeElement = screen.getByLabelText(/We got you covered?/i);
+  expect(homeElement).toBeInTheDocument();
+});
